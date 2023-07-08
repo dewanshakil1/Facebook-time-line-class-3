1248291bee372fb13f267d9a3e598d58e5d11521
unknown <dewanshakin88@gmail.com>
2023-07-08 19:34:31 +0600

    first commit

---
+++ data.js
@@ -0,0 +1,142 @@
+const fbPost=[
+    {
+        id:1,
+        content:"কিছু কিছু জিনিসে থেকে অনেক বিরত থাকতে হবে,,,",
+        like:20,
+        comment:100,
+        photo:"https://scontent.fdac80-1.fna.fbcdn.net/v/t39.30808-6/354056366_289263670228123_4037938216849588646_n.jpg?_nc_cat=106&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeEY4_08_YoVNPb9IaX0G9Z1EmcgJeJbYN8SZyAl4ltg39CGGTPop6iV2cr9CqgClrn4rPv7RHkwcV8t8B9P7cLZ&_nc_ohc=a2k8Mk8rEwYAX-vGlkC&_nc_ht=scontent.fdac80-1.fna&oh=00_AfBerD_isv4o0ITxMoahecw0aB9s_7vxjGVDp3G6Vf5veQ&oe=649F9DBA",
+        author:{
+            
+            name:"Jihad Ahmed",
+            time:10,
+            image:"https://scontent.fdac80-1.fna.fbcdn.net/v/t39.30808-6/342042092_1313640266169737_1529423702092705209_n.jpg?_nc_cat=105&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGfEVMLYSvSFwf9-FC56S3n59ArHWqmTxTn0CsdaqZPFD2lHDfYP4tiPTtUyNoZjiMhAFfUavbqLI9rf2iCIayc&_nc_ohc=QV7JDw1eyRoAX_gvZwK&_nc_ht=scontent.fdac80-1.fna&oh=00_AfDMTDKcYU6A1tvOxnnzsS0ye1DbMyduGRAZSbzWtkWLLA&oe=64A005CA"
+        }
+
+    },
+    {
+        id:2,
+        content:"আমার প্রিয় কুমিল্লা ❤️",
+        like:400,
+        comment:100,
+        photo:"https://scontent.fdac80-1.fna.fbcdn.net/v/t39.30808-6/356385579_229209979921353_4731124921718596769_n.jpg?_nc_cat=1&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFGpvrOc1L8uu79awjaqsQjlyHTptQvgYCXIdOm1C-BgIqSS3kLqN5kx3FXuYyC1QwcU00ievKVh0LUniqkJfMR&_nc_ohc=XvmlXZoSAhUAX-d_dUm&_nc_ht=scontent.fdac80-1.fna&oh=00_AfCQX2uu8mwoAH5bR3-6FWLdnpFLL-p4MQ2obiz4hvIM0A&oe=64A07DD1",
+        author:{
+            
+            name:"𝑇𝑖𝑠𝒉𝑚𝑎 𝑚𝑜𝑛𝑖",
+            time:30,
+            image:"https://www.facebook.com/photo/?fbid=227830613392623&set=a.119512630891089&__tn__=%3C"
+        }
+
+    },
+    {
+        id:3,
+        content:"মুসফিক আর ফারহান ও তানজিন তিশা অভিনীত নাটক সুইট হোম আসছে এই ঈদে",
+        like:500,
+        comment:600,
+        photo:"https://scontent.fdac80-1.fna.fbcdn.net/v/t39.30808-6/356882788_225563890291944_5701762116667510851_n.jpg?_nc_cat=106&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeERWz6SOADhrDlA6CPxwhf6sjK5chZHkgqyMrlyFkeSCqTxjmVrgiW71sMf5_T3IuSg6ulE6HZXeSE4aehCZtPr&_nc_ohc=qIBJqzQ2wzkAX_uZCm_&_nc_ht=scontent.fdac80-1.fna&oh=00_AfBYE813PjS75mJ4LBt6pR4gnKxB-wNiZwnjRX7ai_2LPA&oe=64A148FA",
+        author:{
+            
+            name:"The Farhanians Lover",
+            time:25,
+            image:"https://scontent.fdac80-1.fna.fbcdn.net/v/t39.30808-6/356390972_224407910407542_1966846212071392743_n.jpg?_nc_cat=104&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHOmhtcPw5sCpBp-N7S9uQ4Tepcm4-o0UhN6lybj6jRSCgOQvEO_6XTp0znCNoIiYXI66idwgCBKFTRu_N7xj14&_nc_ohc=HLCHdfxxGigAX-8cSWh&_nc_ht=scontent.fdac80-1.fna&oh=00_AfB7todl7YR7OK9gfKlQay2asqRbm79iSvWm_W-NnSytDA&oe=64A0404B"
+        }
+
+    },
+    {
+        id:4,
+        content:"প্রকৃতির গোপন শক্তিটিকে অর্জন করার চেষ্টা করো । গোপন শক্তিটি হল ধৈর্য ।",
+        like:300,
+        comment:70,
+        photo:"https://scontent-sin6-3.xx.fbcdn.net/v/t39.30808-6/356242213_775969124314966_9132552802931254464_n.jpg?_nc_cat=110&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGuU6Qgvst94JwQmhiDJaF7wicDaBK1UUvCJwNoErVRS5ifNfWkTghnQ-ZYhTJfKN7DzyHGVilVU_Go_PykQ0lk&_nc_ohc=8WVAxHzSJyAAX9KN3zp&_nc_ht=scontent-sin6-3.xx&oh=00_AfA0KuASrdXyf1Rj7ARq_w78T8TzHir0STOgn9EtDbPCAw&oe=64A16B90",
+        author:{
+            
+            name:"Md Ronju Ahmed",
+            time:45,
+            image:"https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/352219126_1594775617674700_1290157329654706879_n.jpg?_nc_cat=102&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeH5CQeO5wq5Wj75bflNXs2ieKUwY5BZGF54pTBjkFkYXrPTHQ7XWq3T9bB5QGP_LfAV6jFxW2jn11VM63LYpIMp&_nc_ohc=JjNQQoQhIMYAX__9Eob&_nc_ht=scontent-sin6-2.xx&oh=00_AfCwRErQcyYmh2AHnYdhYNhbviMzwK-fCRenyGivP2FWFg&oe=64A1B745"
+        }
+
+    },
+    {
+        id:5,
+        content:"",
+        like:300,
+        comment:70,
+        photo:"https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/355099825_776197667625445_6041238678300277213_n.jpg?_nc_cat=105&cb=99be929b-3346023f&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHra3atxfx9PZbWHcL7LVsjI8l7IW6aee0jyXshbpp57Q7w9rn0u1ZN6NRj2VbSWbQ8Yh14Pxe8GYMLOMyBEVTi&_nc_ohc=0XZvQMdxN0oAX83xll2&_nc_ht=scontent-sin6-2.xx&oh=00_AfCZdIzdGh-M40xEXh-KO7kvQvlQqcfxReVf39ixY-hKDA&oe=64A0F79F",
+        author:{
+            
+            name:"Md Ronju Ahmed",
+            time:44,
+            image:"https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-6/352219126_1594775617674700_1290157329654706879_n.jpg?_nc_cat=102&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeH5CQeO5wq5Wj75bflNXs2ieKUwY5BZGF54pTBjkFkYXrPTHQ7XWq3T9bB5QGP_LfAV6jFxW2jn11VM63LYpIMp&_nc_ohc=JjNQQoQhIMYAX__9Eob&_nc_ht=scontent-sin6-2.xx&oh=00_AfCwRErQcyYmh2AHnYdhYNhbviMzwK-fCRenyGivP2FWFg&oe=64A1B745"
+        }
+
+    },
+    {
+        id:6,
+        content:"হকের পথে থাকলে বাঁধা আসবেই হে আল্লাহ ব্রাদার রাহুল হোসেনকে ধৈর্য ধরার তৌফিক দান করুন এবং যাবতীয় বালা মুসিবত থেকে হেফাজত করুন। আমীন❤️",
+        like:330,
+        comment:600,
+        photo:"https://scontent.fdac80-1.fna.fbcdn.net/v/t39.30808-6/347590182_262011139808504_8765485989733291618_n.jpg?_nc_cat=103&cb=99be929b-3346023f&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEOJkOna0RZfez8ut1ZXwT1hU3005NzkcmFTfTTk3ORyT-5rzF5OSzwargrMK_k1pU5dlWJ2bFx_HTs1dwuH9d_&_nc_ohc=B_Zgqpnkvk4AX9dksQQ&_nc_ht=scontent.fdac80-1.fna&oh=00_AfCz6Sp4z-N8whPYo2NB_sSgT5dJANtMsv6g8hozF8ca6Q&oe=64A1B923",
+        author:{
+            
+            name:"Nazmun Nahar",
+            time:44,
+            image:"https://scontent.fdac80-1.fna.fbcdn.net/v/t39.30808-6/278551168_115511761125110_7656564871076112761_n.jpg?_nc_cat=102&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeG33VSh6kSBlabug8jlYfzmoZEvmWh7nBehkS-ZaHucF0TH5zLrAbIJLUIXsHtYYHnSCL6vBguLiWJwPC9d9A-T&_nc_ohc=NNLT9MtJeEIAX_aORiR&_nc_ht=scontent.fdac80-1.fna&oh=00_AfBL_RsIR3gDCz5Gh_XTRlXucspz9Gn7dDLr1akDGaZhHw&oe=64A04413"
+        }
+
+    },
+    {
+        id:7,
+        content:"🌺🌺🌺আমি সেই পাখি,,,যার  বাসা নেই🌸🌸🌸আমি সেই আকাশ,,, যার বুকে চাঁদ নেই🥀🥀🥀আমি সেই সাগর,,, যার তীরে পানি নেই🌹🌹🌹আমি সেই মানুষ💐💐💐যার একটা মন আছে,,,কিন্তু বোঝার মতো মানুষ নেই🌻🌻🌻",
+        like:100,
+        comment:10,
+        photo:"https://scontent.fdac80-1.fna.fbcdn.net/v/t39.30808-6/356829432_272382345445318_8585071546358937358_n.jpg?_nc_cat=110&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFCHoQC02VpR3qW2S4ZitK6XaFyZRpmAWVdoXJlGmYBZQwx4JjnpcLbpnpaQiA3HAFY1laqWTt-MeQSionwKUvb&_nc_ohc=RKR6jOa-OpsAX9Mt-PH&_nc_ht=scontent.fdac80-1.fna&oh=00_AfBtBk3rzn1mayZfvR1F4jlI_txzBHQomfnq3jU35Xcxhg&oe=64A0B06D",
+        author:{
+            
+            name:"Md Akash Mia",
+            time:14,
+            image:"https://scontent.fdac80-1.fna.fbcdn.net/v/t39.30808-6/344801399_974271393728173_2065347383499609865_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=4jia0tO6mCEAX-SplwD&_nc_ht=scontent.fdac80-1.fna&oh=00_AfDQLhYrC3wRc9xA5hUiwg6U2OppgnS4Dsd7wwHOMZ7TPA&oe=64A05A19"
+        }
+
+    },
+    {
+        id:8,
+        content:"-Attitude দেখানোর  মতো চেহারা আমার নাই...!🤦‍♀️",
+        like:100,
+        comment:10,
+        photo:"https://scontent.fdac80-1.fna.fbcdn.net/v/t39.30808-6/332160448_1262918677908187_19868479152549451_n.jpg?_nc_cat=107&cb=99be929b-3346023f&ccb=1-7&_nc_sid=174925&_nc_eui2=AeGa3Gw1EYR2BIabX0_0GMnS-EEk9bRfpMr4QST1tF-kyttwj6D7lDSyUKxxzzHcDcmhzlZu66iK95brVMZ1Sem-&_nc_ohc=6FNpJUax3-EAX-qTduT&_nc_ht=scontent.fdac80-1.fna&oh=00_AfB1h8MtANXL3A5QwIR21V9R_2qFIwFTIVn7jSmR3Pgolg&oe=64A1B7A4",
+        author:{
+            
+            name:"Älåmíñ Hûssāïñ Jūwël",
+            time:14,
+            image:"https://scontent.fdac80-1.fna.fbcdn.net/v/t39.30808-6/352828966_2069103860092635_8225227742178068841_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=azUc9vSvEp4AX822m3Y&_nc_ht=scontent.fdac80-1.fna&oh=00_AfD73hSoPx9dGLmv1aAfj77mdEHwjvL-w1p9v045vv5bZg&oe=64A017DF"
+        }
+
+    },
+    {
+        id:9,
+        content:"“তুমি চাইলেও পেছনে যেতে পারবে না, তবে সামনে না এগিয়ে থেমে আছ কেন?”",
+        like:200,
+        comment:140,
+        photo:"https://www.facebook.com/photo/?fbid=774710447774167&set=pcb.774709174440961&__cft__[0]=AZVQ4iv5xcHNyZ6jrtSjf0jVmdoitc31HfJ1PH48pkWi8SD9TQO4YMG2zL4vRk0pZd0_SlbzOs9TLbsObd-XASJH4NzG1jHYKGgAiaVJctUcle22GEx3EmGxeOS1pcMfMbB4xXZEoNbVGnmFvuCE0HD1hpX9qzOz-2RFiRbllJnxpqbP47_Oe3MxP6b0poAFj-A&__tn__=*bH-R",
+        author:{
+            
+            name:"Md Ronju Ahmed",
+            time:13,
+            image:"https://scontent.fdac80-1.fna.fbcdn.net/v/t39.30808-6/352219126_1594775617674700_1290157329654706879_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=JjNQQoQhIMYAX__9Eob&_nc_ht=scontent.fdac80-1.fna&oh=00_AfBhuk7qDYclN6-T2JGSOHy2tiH0-GNLg5dJJqeeqAxDbg&oe=64A1B745"
+        }
+
+    },
+    {
+        id:10,
+        content:"Make it simple but significant.",
+        like:300,
+        comment:200,
+        photo:"https://scontent.fdac80-1.fna.fbcdn.net/v/t39.30808-6/356019769_1180730176654528_8968725983166682798_n.jpg?_nc_cat=106&cb=99be929b-3346023f&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeH4ZcQt8cleX5_hTCNw-megSvguo_m8THlK-C6j-bxMeWQQp7dV2lBLszxw88rA3r4xt2_L3Jqe04oiZb8vw9Vs&_nc_ohc=l1IkrDoYtIoAX-yo-pr&_nc_ht=scontent.fdac80-1.fna&oh=00_AfDtxgRgs2ZPRgmnwLVOcojt_JZwigpiqETvirnlMKbu9A&oe=64A1A709",
+        author:{
+            
+            name:"Mehedi Hasan Rizve",
+            time:14,
+            image:"https://scontent.fdac80-1.fna.fbcdn.net/v/t39.30808-6/216438971_634453341282217_1846768997593931276_n.jpg?_nc_cat=111&cb=99be929b-3346023f&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHbU1DmE_G4tAu4eyqVjuk3eSC-TwPhrZt5IL5PA-Gtm8M45KfIvB-D8djVzZq40K4S7qFuMGqUJPdnDnOsxV-K&_nc_ohc=b8pT_YD8dnQAX9-l62N&_nc_ht=scontent.fdac80-1.fna&oh=00_AfBIfdzx_h6MPnhXtyD6aIo_jjfh7Igg28auKnr6S0e2tQ&oe=64A0387D"
+        }
+
+    },
+]
